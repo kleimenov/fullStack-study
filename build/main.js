aa64b59a3e4facda2a8274eb65246a344bc99599
Ivan <kleimenov416@gmail.com>
2021-05-05 15:45:33 -0400

    add function that use void data type

--- build/main.js
+++ build/main.js
@@ -1,6 +1,10 @@
 "use strict";
 // import checkType from './module.one/module.one.lesson.three'
-
-// checkType(word)
 var word = 'cat';
+// checkType(word)
 console.log(word);
+var word_second = "Nanana";
+var myFunc = function (student) {
+    console.log(student);
+};
+myFunc(word_second);
--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,10 @@ let word: string = 'cat';
 
 console.log(word)
 
+const word_second = 5;
+
+const myFunc = (student: string | number): void => {
+    console.log(student);
+  };
+
+  myFunc(word_second)
