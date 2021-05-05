9751a791721ab7e18c87dbf5b3813b515b388de2
Ivan <kleimenov416@gmail.com>
2021-05-04 20:11:17 -0400

    start learinig TS

--- module.one/module.one.lesson.three.js
+++ module.one/module.one.lesson.three.js
@@ -1,1 +1,23 @@
 //Topic of this lesson: Developing with typeScript
+
+//in the beggining lie's play with types and spot how dump JS recognize data types
+let a = 44;
+let b = 10;
+let stringNum = '3';
+let word = 'XYI';
+
+const addNumbers = (var1, var2) => {
+   console.log(var1 + var2)
+}
+
+const multipleNumbers = (var1, var2) => {
+    console.log(var1 * var2)
+}
+
+addNumbers(a, b)
+addNumbers(a, stringNum)
+addNumbers(word, b)
+
+multipleNumbers(a, b)
+multipleNumbers(b, stringNum)
+multipleNumbers(word, b)
---
+++ module.one/module.one.lesson.three.ts
@@ -0,0 +1,24 @@
+//let's explore magic, here I will learn through the lengs of TS magic,
+//how it works with different types of data
+//SPOILER ALERT better that JS:)
+
+// let variable_I  = 44;
+// let variable_II  = 10;
+// let variable_III = '3';
+// let variable_IV = 'XYI';
+
+// const functionOne = (var1:number , var2:number) => {
+//    console.log(var1 + var2)
+// }
+
+// const functionTwo = (var1, var2) => {
+//     console.log(var1 * var2)
+// }
+
+// functionOne(variable_I, variable_II)
+// functionOne(variable_I, variable_III)
+// functionOne(variable_IV, variable_II)
+// console.log('-----------------------------------')
+// functionTwo(variable_I, variable_II)
+// functionTwo(variable_I, variable_III)
+// functionTwo(variable_IV, variable_II)
