3bfc037476db5def8049beef6cfd3d15eb60cd67
Ivan <kleimenov416@gmail.com>
2021-05-05 10:44:37 -0400

    add some compile settings inside tsconfig

---
+++ build/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var variable_I = 44;
+var variable_II = 10;
+var variable_III = '3';
+var variable_IV = 'XYI';
+var functionOne = function (var1, var2) {
+    console.log(var1 + var2);
+};
+functionOne(variable_I, variable_II);
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+let variable_I  = 44;
+let variable_II  = 10;
+let variable_III = '3';
+let variable_IV = 'XYI';
+const functionOne = (var1:number , var2:number) => {
+   console.log(var1 + var2)
+}
+
+functionOne(variable_I, variable_II)
